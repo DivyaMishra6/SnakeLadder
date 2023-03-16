56ceafe7dd31a6e5e29a6d9175c2bbc915fcab5b
DivyaMishra6 <divyamishra599@gmail.com>
2023-03-16 21:42:53 +0530

    Single player start with Zero

---
+++ SinglePlayerUC1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadder
+{
+    public class SinglePlayerUC1
+    {
+        public static void call()
+        {
+            Console.WriteLine("Start the Snake and Ladder Game");
+            Console.WriteLine("Single Player at Start Position 0");
+        }
+    }
+}
