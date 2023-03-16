a6154344a35aada1d2e9b4aaadde2a95a663cf44
DivyaMishra6 <divyamishra599@gmail.com>
2023-03-16 21:46:22 +0530

    Roll Dice

---
+++ RollDiceUC2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadder
+{
+    public class RollDiceUC2
+    {
+        public static void call()
+        {
+            Console.WriteLine("Start the Snake and Ladder Game");
+            Console.WriteLine("Single Player at Start Position 0");
+            Random random = new Random();
+            int RollDice = random.Next(1, 6);
+            Console.WriteLine("Player 1 rolls the die and get:" + RollDice);
+        }
+    }
+}
