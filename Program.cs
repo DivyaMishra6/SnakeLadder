eeea1f831049406de4ac090a9ecb335385636b4f
DivyaMishra6 <divyamishra599@gmail.com>
2023-03-16 21:38:53 +0530

    Welcome To Snake Ladder Program

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace SnakeLadder
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // SinglePlayerUC1.call();
+            //RollDiceUC2.call();
+             //CheckOptionUC3.call();
+            // ReachTillMaxUC4.call();
+            //CheckNotAboveMaxUC5.call();
+            //RollCountUC6.call();
+            PlayerWonGameUC7.call();
+
+        }
+    }
+}
